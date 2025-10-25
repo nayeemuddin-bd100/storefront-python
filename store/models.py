f0745eeb8356578be3b299fc2fc47b4f67887ac1
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2025-10-25 23:30:49 +0600

    Add initial models and migration for Customer and Product

--- store/models.py
+++ store/models.py
@@ -8,3 +8,28 @@ class Product(models.Model):
     description = models.TextField()  # text
     price = models.DecimalField(
         max_digits=10, decimal_places=2)  # decimal(10,2)
+    inventory = models.IntegerField()  # integer
+    last_update = models.DateTimeField(auto_now=True)  # timestamp
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, )
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1,
+        choices=MEMBERSHIP_CHOICES,
+        default=MEMBERSHIP_BRONZE,
+    )
